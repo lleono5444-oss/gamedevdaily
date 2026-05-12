0346d218df45cfe8857c0266f45fd1b2403bc6f5
lleono5444-oss <lleono5444@gmail.com>
2026-05-13 02:09:04 +0900

    Create 특수문자출력하기.cs

---
+++ algo/Programgers/Programgers/특수문자출력하기.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Example5
+{
+    public static void Main()
+    {
+        Console.WriteLine($"!@#$%^&*(\\'\" <>?:; ");
+    }
+}
+/*
+ *!@#$%^&*(\'"<>?:; 이 문자 출력
+ * \을 앞에 붙여서 출력되게
+ * 
+ * 문제와 별개
+ * 형변환(int) 와 TryParse 차이
+ * 
+ * 앞은 타입만 변환 뒤는 문자열 해석
+ */
+
