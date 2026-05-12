8b26c8ec3398d9d7c7fca64733655badd2110da2
lleono5444-oss <lleono5444@gmail.com>
2026-05-12 16:30:21 +0900

    Create Program.cs

---
+++ algo/Programgers/Programgers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programgers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
