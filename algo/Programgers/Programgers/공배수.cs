c096eeb75d58bb31ff27a91fa0b49dea0412302b
lleono5444-oss <lleono5444@gmail.com>
2026-05-13 03:17:58 +0900

    Create 공배수.cs

---
+++ algo/Programgers/Programgers/공배수.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Solution1
+{
+    public int solution(int number, int n, int m)
+    {
+        int answer = 0;
+
+        if (number % n == 0 && number % m == 0)
+        {
+            answer = 1;
+        }
+
+        return answer;
+
+    }
+}
+
+/*
+ * 
+
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        return number%n == 0 && number%m == 0 ? 1 : 0;
+    }
+}
+삼항 연산자를 사용한 풀이
+ */
