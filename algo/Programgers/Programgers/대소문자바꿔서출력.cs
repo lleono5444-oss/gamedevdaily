d0fbe75d12bda3c94fe0c3fde5db089fe8df114c
lleono5444-oss <lleono5444@gmail.com>
2026-05-13 02:19:48 +0900

    Update 대소문자바꿔서출력.cs

--- algo/Programgers/Programgers/대소문자바꿔서출력.cs
+++ algo/Programgers/Programgers/대소문자바꿔서출력.cs
@@ -13,13 +13,43 @@ public class Example
        
         for (int i = 0; i < s.Length; i++)
         {
-            if (char.IsLower(s[i]) sb.Append(char.ToUpper(s[i])); 
-            else sb.Append(char.ToLower(s[i])); ;
+            if (char.IsLower(s[i])) sb.Append(char.ToUpper(s[i])); 
+            else sb.Append(char.ToLower(s[i]));
        
         }
         Console.WriteLine(sb);
     }
 }
+
+/*다른 사람 풀이
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+
+        foreach (char c in s) //foreach문 연습 많이 해보기
+        {
+            if (char.IsUpper(c))
+            {
+                Console.Write(char.ToLower(c));
+            }
+            else
+            {
+                Console.Write(char.ToUpper(c));
+            }
+        }
+    }
+}
+
+
+너무 StringBuilder에 매몰 되지는 말고 풀 수 있는 방법이 있다면 다 써보는 식으로 진행하자
+*/
 
 //char.ToUpper(ch)
 //char.ToLower(ch)
