dabea09ec968c4030a01cade90b611a5f319fa37
lleono5444-oss <lleono5444@gmail.com>
2026-05-13 03:17:55 +0900

    Create 문자열붙여서출력.cs

---
+++ algo/Programgers/Programgers/문자열붙여서출력.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Example10
+{
+    public static void Main10()
+    {
+        String[] input;
+
+        Console.Clear();
+        input = Console.ReadLine().Split(' ');
+
+        String s1 = input[0];
+        String s2 = input[1];
+
+        Console.WriteLine($"{s1}{s2}");
+
+    }
+}
+
+/*다른 사람 풀이
+
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] input;
+
+        Console.Clear();
+        input = Console.ReadLine().Split(' ');
+
+        String s1 = input[0];
+        String s2 = input[1];
+        string sum = s1+s2;
+        sum = sum.Trim();
+        Console.WriteLine(sum);
+
+    }
+}
+TrimStart(),TrimEnd(),Trim()
+각각 시작 공백 제거,마지막 공백 제거,양쪽 공백 제거 기능
+
+ */
